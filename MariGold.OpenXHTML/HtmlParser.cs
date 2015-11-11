88ecbe09cff0c2ed1a8f769b2e45c99ecd244c56
Kannan Ramachandran <kannanpala@gmail.com>
2015-11-11 21:48:34 +0530

    Interfaces added

---
+++ MariGold.OpenXHTML/HtmlParser.cs
@@ -0,0 +1,15 @@
+﻿namespace MariGold.OpenXHTML
+{
+	using System;
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public class HtmlParser : IHtmlParser
+	{
+		public IHtmlElement FindBodyOrFirstElement()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
