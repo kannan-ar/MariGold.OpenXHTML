7ac2bdb8652be84a0c956ba5942a31ebb67fa72a
Kannan Ramachandran <kannanpala@gmail.com>
2016-08-31 22:55:55 +0530

    Bug fixes

---
+++ MariGold.OpenXHTML.Tests/InvalidElements.cs
@@ -0,0 +1,54 @@
+﻿namespace MariGold.OpenXHTML.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using MariGold.OpenXHTML;
+    using System.IO;
+    using DocumentFormat.OpenXml;
+    using DocumentFormat.OpenXml.Wordprocessing;
+    using Word = DocumentFormat.OpenXml.Wordprocessing;
+    using DocumentFormat.OpenXml.Validation;
+    using System.Linq;
+
+    [TestFixture]
+    public class InvalidElements
+    {
+        [Test]
+        public void ScriptElement()
+        {
+            using (MemoryStream mem = new MemoryStream())
+            {
+                string html = "<script type=\"text/javascript\">console.log('ok');</script>";
+                WordDocument doc = new WordDocument(mem);
+
+                doc.Process(new HtmlParser(html));
+
+                Assert.IsNotNull(doc.Document.Body);
+                Assert.AreEqual(0, doc.Document.Body.ChildElements.Count);
+
+                OpenXmlValidator validator = new OpenXmlValidator();
+                var errors = validator.Validate(doc.WordprocessingDocument);
+                Assert.AreEqual(0, errors.Count());
+            }
+        }
+
+        [Test]
+        public void StyleElement()
+        {
+            using (MemoryStream mem = new MemoryStream())
+            {
+                string html = "<style type=\"text/css\">.cls{font-size:10px;}</style>";
+                WordDocument doc = new WordDocument(mem);
+
+                doc.Process(new HtmlParser(html));
+
+                Assert.IsNotNull(doc.Document.Body);
+                Assert.AreEqual(0, doc.Document.Body.ChildElements.Count);
+
+                OpenXmlValidator validator = new OpenXmlValidator();
+                var errors = validator.Validate(doc.WordprocessingDocument);
+                Assert.AreEqual(0, errors.Count());
+            }
+        }
+    }
+}
--- MariGold.OpenXHTML/Elements/DocxInline.cs
+++ MariGold.OpenXHTML/Elements/DocxInline.cs
@@ -7,11 +7,26 @@
 
     internal sealed class DocxInline : DocxElement, ITextElement
     {
+        private readonly string[] nonTextTags = { "script", "style" };
+
+        private bool IsTextTag(string tag)
+        {
+            foreach (string nonTextTag in nonTextTags)
+            {
+                if (string.Compare(tag, nonTextTag, true) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         internal DocxInline(IOpenXmlContext context) : base(context) { }
 
         internal override bool CanConvert(DocxNode node)
         {
-            return true;//It can convert anything!!!
+            return IsTextTag(node.Tag);
         }
 
         internal override void Process(DocxNode node, ref Paragraph paragraph)
