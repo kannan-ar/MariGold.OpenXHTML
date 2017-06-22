36b387f3455f9ac59f6dad7a37738fdc3d232554
Kannan Ramachandran <kannanpala@gmail.com>
2017-06-22 19:43:37 +0530

    Version 1.1 released

--- MariGold.OpenXHTML/Elements/DocxImage.cs
+++ MariGold.OpenXHTML/Elements/DocxImage.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Net;
     using System.IO;
-    using MariGold.HtmlParser;
     using DocumentFormat.OpenXml;
     using DocumentFormat.OpenXml.Packaging;
     using DocumentFormat.OpenXml.Wordprocessing;
