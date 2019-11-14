2c4f365f8404fac38e6b0ffda4624858a4382f25
Kannan Ramachandran <kannanpala@gmail.com>
2019-11-14 11:28:58 +0530

    Bug fixes

--- MariGold.OpenXHTML.Tests/InvalidElements.cs
+++ MariGold.OpenXHTML.Tests/InvalidElements.cs
@@ -5,6 +5,7 @@
     using System.IO;
     using DocumentFormat.OpenXml.Validation;
     using System.Linq;
+    using Word = DocumentFormat.OpenXml.Wordprocessing;
 
     [TestFixture]
     public class InvalidElements
@@ -40,6 +41,54 @@
 
                 Assert.IsNotNull(doc.Document.Body);
                 Assert.AreEqual(0, doc.Document.Body.ChildElements.Count);
+
+                OpenXmlValidator validator = new OpenXmlValidator();
+                var errors = validator.Validate(doc.WordprocessingDocument);
+                Assert.AreEqual(0, errors.Count());
+            }
+        }
+
+        [Test]
+        public void InvalidHr()
+        {
+            using (MemoryStream mem = new MemoryStream())
+            {
+                WordDocument doc = new WordDocument(mem);
+
+                doc.Process(new HtmlParser("<div>1</div><hr><div>2</div>"));
+
+                Assert.IsNotNull(doc.Document.Body);
+                Assert.AreEqual(3, doc.Document.Body.ChildElements.Count);
+
+                Word.Paragraph para = doc.Document.Body.ChildElements[0] as Word.Paragraph;
+
+                Assert.IsNotNull(para);
+                Assert.AreEqual(1, para.ChildElements.Count);
+
+                Word.Run run = para.ChildElements[0] as Word.Run;
+
+                Assert.IsNotNull(run);
+                Assert.AreEqual(1, run.ChildElements.Count);
+
+                Word.Text text = run.ChildElements[0] as Word.Text;
+
+                Assert.IsNotNull(text);
+                Assert.AreEqual("1", text.InnerText);
+
+                para = doc.Document.Body.ChildElements[2] as Word.Paragraph;
+
+                Assert.IsNotNull(para);
+                Assert.AreEqual(1, para.ChildElements.Count);
+
+                run = para.ChildElements[0] as Word.Run;
+
+                Assert.IsNotNull(run);
+                Assert.AreEqual(1, run.ChildElements.Count);
+
+                text = run.ChildElements[0] as Word.Text;
+
+                Assert.IsNotNull(text);
+                Assert.AreEqual("2", text.InnerText);
 
                 OpenXmlValidator validator = new OpenXmlValidator();
                 var errors = validator.Validate(doc.WordprocessingDocument);
